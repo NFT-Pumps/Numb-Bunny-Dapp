850d5ca20b2450a790c80893a3a2dd0ae780c022
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-10-22 13:13:59 -0500

    Update

--- components/banner/MintPage.js
+++ components/banner/MintPage.js
@@ -95,10 +95,10 @@ const MintPage = () => {
             {(!currentUseState.isConnected) ?
               <Col lg="6" md="6" className="align-self-center">
                 {(currentUseState.network == "rinkeby") ? <h3 style={{ color: "#fff" }}>DEMO ONLY RINKEBY</h3> : ""}
-                <h3 className="title" style={{ color: "#a938c2"}}>
+                <h3 className="title" style={{ color: "#ffffff"}}>
                   { process.env.pageHeader }
                 </h3>
-                <h4 className="subtitle font-light" style={{ color: "#a938c2"}}>
+                <h4 className="subtitle font-light" style={{ color: "#ffffff"}}>
                   { process.env.pageText }
                   <br />
                 </h4>
@@ -167,7 +167,7 @@ const MintPage = () => {
                       {currentUseState.hashHtml}
                     </>
                     :
-                    <h1 className="subtitle font-light" style={{ color: "#a938c2"}}>Public mint is currently closed!</h1>
+                    <h1 className="subtitle font-light" style={{ color: "#ffffff"}}>Public mint is currently closed!</h1>
                   }
                 </>
               </Col>
