e45a9c1e66b4bb481596f817fcdf30c6e779debe
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-10-28 18:23:27 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
     mintType : "Pre-Sale", //Pre-Sale Public
     enforceWhitelist : true,
     ethValue : "0.02",
-    ethWLValue : "0.00",
+    ethWLValue : "0.01",
     debug: false,
     network: 'mainnet',
     chainId: 1,
